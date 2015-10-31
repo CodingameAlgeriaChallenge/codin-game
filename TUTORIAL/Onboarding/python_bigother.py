4dd270ffaeeabc5a6feed93705857200d274fcad
Assem Chelli / bigOTHER <assem.ch@gmail.com>
2015-10-31 21:17:48 +0100

    Python solution for Tutorial - Onboarding

---
+++ TUTORIAL/Onboarding/python_bigother.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+
+# game loop
+while 1:
+    enemy_1 = raw_input() # name of enemy 1
+    dist_1 = int(raw_input()) # distance to enemy 1
+    enemy_2 = raw_input() # name of enemy 2
+    dist_2 = int(raw_input()) # distance to enemy 2
+
+    print enemy_1 if dist_1 < dist_2  else enemy_2
